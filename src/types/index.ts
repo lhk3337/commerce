7b233a858f6842e6845493681035858471d0c735
lhk3337 <lhk3337@naver.com>
2022-02-09 18:36:23 +0900

    store type, connect mapDispatchToProps type 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,17 @@
+// redux/store.ts
+import { Btn1, Btn2, Btn3, Btn4, Btn5 } from "../redux/store";
+export type NumberAction =
+  | ReturnType<typeof Btn1>
+  | ReturnType<typeof Btn2>
+  | ReturnType<typeof Btn3>
+  | ReturnType<typeof Btn4>
+  | ReturnType<typeof Btn5>;
+
+export type StateType = {
+  number: number;
+};
+
+// Component/Slider.tsx
 export interface ProductItem {
   id: number;
   title: string;
@@ -16,6 +30,26 @@ export interface FeatureItem {
   desc: string;
 }
 
+// Component/Feature.tsx
 export interface FeatureType {
   features: FeatureItem[];
+}
+
+// Component/Header.tsx
+export interface IBtn {
+  Btn1: () => {
+    type: string;
+  };
+  Btn2: () => {
+    type: string;
+  };
+  Btn3: () => {
+    type: string;
+  };
+  Btn4: () => {
+    type: string;
+  };
+  Btn5: () => {
+    type: string;
+  };
 }
