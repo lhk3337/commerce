490e9843f3bb0e220fbad380735502b2747291df
lhk3337 <lhk3337@naver.com>
2022-02-11 11:49:55 +0900

    Product Component reducer 추가

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-
+import products from "../assets/data/products";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { NumberAction, StateType } from "../types";
 const BTN1 = "BTN1" as const;
@@ -16,20 +16,23 @@ export const Btn5 = () => ({ type: "BTN5" });
 
 const initialState: StateType = {
   number: 0,
+  price: products[0].price,
+  title: products[0].title,
+  colors: products[0].colors,
 };
 
 const reducer = (state: StateType = initialState, action: NumberAction) => {
   switch (action.type) {
     case BTN1:
-      return { number: 0 };
+      return { number: 0, price: products[0].price, title: products[0].title, colors: products[0].colors };
     case BTN2:
-      return { number: -100 };
+      return { number: -100, price: products[1].price, title: products[1].title, colors: products[1].colors };
     case BTN3:
-      return { number: -200 };
+      return { number: -200, price: products[2].price, title: products[2].title, colors: products[2].colors };
     case BTN4:
-      return { number: -300 };
+      return { number: -300, price: products[3].price, title: products[3].title, colors: products[3].colors };
     case BTN5:
-      return { number: -400 };
+      return { number: -400, price: products[4].price, title: products[4].title, colors: products[4].colors };
     default:
       return state;
   }
