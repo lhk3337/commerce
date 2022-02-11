4b741d1a4b5444d6d23a4cee6223dfa008080423
lhk3337 <lhk3337@naver.com>
2022-02-11 16:32:30 +0900

    Product Component buy modal 추가

--- src/Components/Product.tsx
+++ src/Components/Product.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import * as S from "Style/Product";
 import { useSelector, connect } from "react-redux";
 import { navMenuType, Icolor } from "types";
+import visa from "assets/img/visa.png";
+import master from "assets/img/master.png";
 
 const Product = ({ title, price, colors }: any) => {
   const [imgUrl, setImgUrl] = useState(0);
@@ -9,9 +11,13 @@ const Product = ({ title, price, colors }: any) => {
   const [boolColor1, setBoolColor1] = useState(true);
   const [boolColor2, setBoolColor2] = useState(true);
   const [boolColor3, setBoolColor3] = useState(true);
+
+  const [modal, setModal] = useState(false);
+
   useEffect(() => {
     setImgUrl(0);
   }, [colors]);
+
   const onClickImg = (index: number) => {
     setImgUrl(index);
   };
@@ -30,6 +36,14 @@ const Product = ({ title, price, colors }: any) => {
       setBoolColor2(true);
       setBoolColor3(false);
     }
+  };
+
+  const openClickModal = () => {
+    setModal(true);
+  };
+
+  const closeClickModal = () => {
+    setModal(false);
   };
 
   return (
@@ -58,7 +72,32 @@ const Product = ({ title, price, colors }: any) => {
             44
           </S.Size>
         </S.Sizes>
+        <S.BuyBtn onClick={openClickModal}>BUY NOW!</S.BuyBtn>
       </S.ProductDetails>
+      {modal ? (
+        <S.Payment>
+          <S.PayTitle>Personal Information</S.PayTitle>
+          <label>Name and Surname</label>
+          <S.PayInput type="text" placeholder="John Doe" />
+          <label>Phone Number</label>
+          <S.PayInput type="text" placeholder="+1 234 5678" />
+          <label>Address</label>
+          <S.PayInput type="text" placeholder="Elton St 21 22-145" />
+          <S.PayTitle>Card Information</S.PayTitle>
+          <S.CardIcons>
+            <S.CardIcon src={visa} />
+            <S.CardIcon src={master} />
+          </S.CardIcons>
+          <S.PayInput type="password" placeholder="Card Number" />
+          <S.CardInfo>
+            <S.PayInputSm type="text" placeholder="mmm" />
+            <S.PayInputSm type="text" placeholder="yyyy" />
+            <S.PayInputSm type="text" placeholder="cvv" />
+          </S.CardInfo>
+          <S.PayButton>Checkout!</S.PayButton>
+          <S.Close onClick={closeClickModal}>X</S.Close>
+        </S.Payment>
+      ) : null}
     </S.Container>
   );
 };
--- src/Style/Product.ts
+++ src/Style/Product.ts
@@ -58,3 +58,92 @@ export const Sizes = styled.div`
   display: flex;
   margin-bottom: 20px;
 `;
+
+export const BuyBtn = styled.button`
+  float: right;
+  padding: 10px 20px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+  cursor: pointer;
+  &:hover {
+    background-color: white;
+    color: black;
+  }
+`;
+
+export const Payment = styled.div`
+  width: 550px;
+  height: 530px;
+  background-color: white;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  padding: 10px 50px;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-shadow: 0px 0px 13px 2px rgba(0, 0, 0, 0.3);
+  box-shadow: 0px 0px 13px 2px rgba(0, 0, 0, 0.3);
+`;
+
+export const PayTitle = styled.h1`
+  font-size: 20px;
+  color: lightgray;
+`;
+
+export const PayInput = styled.input`
+  padding: 10px;
+  margin: 10px 0px;
+  border: none;
+  border-bottom: 1px solid gray;
+  &::placeholder {
+    color: rgb(163, 163, 163);
+  }
+`;
+
+export const CardIcons = styled.div`
+  display: flex;
+`;
+
+export const CardIcon = styled.img`
+  margin-right: 10px;
+  width: 40px;
+`;
+export const CardInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export const PayInputSm = styled(PayInput)`
+  width: 30%;
+`;
+export const PayButton = styled.button`
+  position: absolute;
+  height: 40px;
+  bottom: -40px;
+  width: 100%;
+  left: 0;
+  -webkit-box-shadow: 0px 0px 13px 2px rgba(0, 0, 0, 0.3);
+  box-shadow: 0px 0px 13px 2px rgba(0, 0, 0, 0.3);
+  background-color: #369e62;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
+
+export const Close = styled.span`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  background-color: gray;
+  color: white;
+  top: 10px;
+  right: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  padding: 2px;
+`;
